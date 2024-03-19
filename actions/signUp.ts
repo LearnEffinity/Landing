f1b946d11fd1fa2067e7b7875a270202f08bcdfb
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-03-19 11:44:07 -0700

    Refactor signUp function to use consistent formatting

--- actions/signUp.ts
+++ actions/signUp.ts
@@ -1,19 +1,18 @@
-'use server'
+"use server";
 import { Resend } from "resend";
 
-export async function signUp (formData: FormData) {
-    const email = formData.get('email');
+export async function signUp(formData: FormData) {
+  const email = formData.get("email");
 
-    const resend = new Resend(process.env.RESEND_API_KEY);
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
-    try {
-        const { data } = await resend.contacts.create({
-            email: email as string,
-            audienceId: process.env.RESEND_AUD as string,
-        });
-        console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
-
+  try {
+    const { data } = await resend.contacts.create({
+      email: email as string,
+      audienceId: process.env.RESEND_AUD as string,
+    });
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
