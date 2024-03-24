90d3e79594ef8feff8f025bb9fececc3f50ec628
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-03-24 01:28:36 -0700

    Refactor CallToAction component to improve readability and performance

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -13,7 +13,7 @@ export default function CallToAction() {
 
   useEffect(() => {
     const handleResize = () => {
-        setIsMobile(window.matchMedia("(max-width: 430px)").matches);
+      setIsMobile(window.matchMedia("(max-width: 430px)").matches);
     };
 
     handleResize();
@@ -21,43 +21,42 @@ export default function CallToAction() {
     window.addEventListener("resize", handleResize);
 
     return () => {
-        window.removeEventListener("resize", handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   useGSAP(() => {
-      if (isMobile){
-        gsap.to("#rocket", { 
-          duration: 2,
-          y: -10,
-          yoyo: true,
-          repeat: -1,
-          ease: "sine.inOut",
-          scrollTrigger: {
-            trigger: "#callToAction",
-            start: "top bottom",
-            toggleActions: "play pause play pause",
-          }
-        });
-      }
-      else {
-        gsap.to("#rocket", { 
-          duration: 2.5,
-          x: -20,
-          y: -20,
-          yoyo: true,
-          repeat: -1,
-          ease: "sine.inOut",
-          scrollTrigger: {
-            trigger: "#callToAction",
-            start: "top bottom",
-            toggleActions: "play pause play pause",
-          }
-        });
-      }
-      
-      if (!isMobile){
-      gsap.to("#atom", { 
+    if (isMobile) {
+      gsap.to("#rocket", {
+        duration: 2,
+        y: -10,
+        yoyo: true,
+        repeat: -1,
+        ease: "sine.inOut",
+        scrollTrigger: {
+          trigger: "#callToAction",
+          start: "top bottom",
+          toggleActions: "play pause play pause",
+        },
+      });
+    } else {
+      gsap.to("#rocket", {
+        duration: 2.5,
+        x: -20,
+        y: -20,
+        yoyo: true,
+        repeat: -1,
+        ease: "sine.inOut",
+        scrollTrigger: {
+          trigger: "#callToAction",
+          start: "top bottom",
+          toggleActions: "play pause play pause",
+        },
+      });
+    }
+
+    if (!isMobile) {
+      gsap.to("#atom", {
         delay: 0.5,
         duration: 2.5,
         y: -20,
@@ -68,15 +67,14 @@ export default function CallToAction() {
           trigger: "#callToAction",
           start: "top bottom",
           toggleActions: "play pause play pause",
-        } 
+        },
       });
     }
-
   }, [isMobile]);
 
   const { scrollYProgress } = useScroll({
     target: container,
-    offset: ["start start", "start end"]
+    offset: ["start start", "start end"],
   });
 
   const bushY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
@@ -85,7 +83,7 @@ export default function CallToAction() {
     <>
       <div className="flex flex-col lg:flex-row mt-10" id="#callToAction">
         <div className="mx-auto h-full w-full hidden lg:block " ref={bush}>
-          <div className="inset-y-0 left-0 mt-28" id='atom' >
+          <div className="inset-y-0 left-0 mt-28" id="atom">
             <Image
               src="/cta/atom.png"
               width={272}
@@ -110,9 +108,13 @@ export default function CallToAction() {
               </h1>
             </div>
           </div>
-          <SignUpForm/>
+          <SignUpForm />
         </div>
-        <div className="mx-auto h-full w-full flex justify-center" id='rocket' ref={container}>
+        <div
+          className="mx-auto h-full w-full flex justify-center"
+          id="rocket"
+          ref={container}
+        >
           <div className="lg:inset-y-0 lg:right-0 lg:mr-0 mr-6">
             <Image
               src="/cta/rocket.png"
@@ -125,7 +127,7 @@ export default function CallToAction() {
           </div>
         </div>
       </div>
-      <motion.div style={{y:bushY}}>
+      <motion.div style={{ y: bushY }}>
         <Image
           src="/cta/person-bush.png"
           width={1248}
@@ -134,11 +136,8 @@ export default function CallToAction() {
           alt="atom"
           className="mt-25"
           id="bush"
-          />
+        />
       </motion.div>
     </>
   );
 }
-
-
-
