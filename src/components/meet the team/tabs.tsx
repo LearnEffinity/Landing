c0a6ac23065c3718da1d243f354d02bee635600e
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-03-24 14:27:06 -0700

    Refactor team tabs component layout and styles

--- src/components/meet the team/tabs.tsx
+++ src/components/meet the team/tabs.tsx
@@ -12,8 +12,8 @@ const ChipTabs = ({
 }) => {
   return (
     <div className="flex justify-center w-full ">
-      <div className=" bg-surface-secondary flex justify-center items-center w-[740px] h-[76px] rounded-xl mb-10">
-        <div className="flex justify-center gap-5 ">
+      <div className=" bg-surface-secondary flex justify-center items-center w-[365px] h-[52px] md:w-[740px] md:h-[76px] rounded-xl mb-10">
+        <div className="flex justify-center gap-5 px-4 md:px-0">
           {tabs.map((tab) => (
             <Chip
               text={tab}
@@ -44,14 +44,16 @@ const Chip = ({
         selected
           ? "text-white"
           : "text-primary hover:text-primary/90 hover:bg-gray-500/10 transition-colors duration-200 ease-in-out"
-      } text-sm transition-colors rounded-md relative w-[213px] h-[52px]`}
+      } text-sm transition-colors rounded-md relative w-[104px] h-[36px] md:w-[213px] md:h-[52px]`}
     >
-      <span className="relative z-10 text-lg font-medium">{text}</span>
+      <span className="relative z-10 text-md  md:text-lg font-medium">
+        {text}
+      </span>
       {selected && (
         <motion.span
           layoutId="pill-tab"
           transition={{ type: "spring", duration: 0.5 }}
-          className="absolute inset-0 z-0 bg-brand-accent w-[213px] h-[52px] text-center rounded-md"
+          className="absolute inset-0 z-0 bg-brand-accent w-[104px] h-[36px] md:w-[213px] md:h-[52px] text-center rounded-md"
         ></motion.span>
       )}
     </button>
