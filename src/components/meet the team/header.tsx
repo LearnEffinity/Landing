b1139140c4749210d04367a5649b265bec0e8935
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-03-24 01:29:14 -0700

    Add Meet the Team header component

---
+++ src/components/meet the team/header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <h1 className="bg-gradient-to-r mt-4 from-decorative-blue to-decorative-red bg-clip-text text-transparent text-title font-semibold text-center">
+        Meet the Team
+      </h1>
+      <p className="mb-14 text-center font-medium text-lg text-primary">
+        The team behind empowering the next generation&apos;s financial future.
+      </p>
+    </>
+  );
+}
