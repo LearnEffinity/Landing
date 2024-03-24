887087e98b588ed24fbec37817d272ad75df4bf1
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-03-24 14:27:04 -0700

    Update header styles

--- src/components/meet the team/header.tsx
+++ src/components/meet the team/header.tsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Header() {
   return (
     <>
-      <h1 className="bg-gradient-to-r mt-4 from-decorative-blue to-decorative-red bg-clip-text text-transparent text-title font-semibold text-center">
+      <h1 className="bg-gradient-to-r from-decorative-blue to-decorative-red bg-clip-text text-transparent text-[24px] sm:text-title font-semibold text-center">
         Meet the Team
       </h1>
-      <p className="mb-14 text-center font-medium text-lg text-primary">
+      <p className="mb-8 md:mb-14 text-center font-medium text-[16px] md:text-lg text-primary">
         The team behind empowering the next generation&apos;s financial future.
       </p>
     </>
