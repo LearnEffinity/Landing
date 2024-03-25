cacaa52719cdce8946a1584103e9cfcd2bc14c7b
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-03-25 13:08:06 -0700

    Add MeetTheTeam component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import CallToAction from "@/components/CallToAction";
+import MeetTheTeam from "@/components/meet the team";
 
 export default function Home() {
   return (
     <>
+      <MeetTheTeam />
       <CallToAction />
     </>
   );
