885f588b893ab79130c120b511e88e206a808d19
Razeen <razeen_ali@yahoo.com>
2024-03-25 23:16:21 -0400

    Fixed positioning, text wrapping/responsiveness

--- src/app/heroSection.tsx
+++ src/app/heroSection.tsx
@@ -1,53 +1,55 @@
 "use client";
 import React from "react";
 import "./heroSection.css";
-import { motion } from "framer-motion";
 import SignUpButton from "@/components/SignUpForm";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="hero-section">
+    <div className="hero-section" style={{ height: "calc(100vh + 2px)" }}>
       <div className="content-container">
-        <h1 className="text-4xl lg:text-5xl font-semibold leading-tight text-center mx-auto px-4 lg:px-0 max-w-screen-lg">
-          <span>Unlock Your Financial Potential,</span>
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">
+        <h1 className="title-h1 text-4xl leading-normal lg:text-5xl font-semibold  text-center mx-auto px-4 lg:px-0 max-w-screen-lg title-with-gradient">
+          <span className="title-span-non">
+            Unlock Your Financial Potential,
+          </span>
+          <span className="title-s2 title-span ">
             Gamify Your Learning Journey
           </span>
         </h1>
-        <p className="text-base lg:text-lg text-center mx-auto px-4 lg:px-0">
+        <p className="subtitle text-base lg:text-lg text-center mx-auto px-4 lg:px-0 mt-4 lg:mt-6 max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-3xl whitespace-normal">
           Say goodbye to boring finance lessons and get ahead financially while
           having a blast with Effinity!
         </p>
+
         <SignUpButton />
       </div>
       <img
         src="/heroIcons/Board.png"
-        alt="Descriptive alt text"
+        alt="Board icon"
         className="backdrop board"
       />
       <img
         src="/heroIcons/trophy.png"
-        alt="Descriptive alt text"
+        alt="Trohpy icon"
         className="backdrop trophy"
       />
       <img
         src="/heroIcons/Online-Study.png"
-        alt="Descriptive alt text"
+        alt="Study icon"
         className="backdrop study"
       />
       <img
         src="/heroIcons/Star-1.png"
-        alt="Descriptive alt text"
+        alt="Star icon"
         className="backdrop star"
       />
       <img
         src="/heroIcons/Calculator-1.png"
-        alt="Descriptive alt text"
+        alt="calculator icon"
         className="backdrop calculator"
       />
       <img
         src="/heroIcons/Card-1.png"
-        alt="Descriptive alt text"
+        alt="Card icon"
         className="backdrop card"
       />
     </div>
