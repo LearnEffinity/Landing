3cf317fe969f7cc921a1a824582a084ac16b3c8b
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-03-25 17:34:33 -0700

    Wiggle animation

--- src/app/heroSection.tsx
+++ src/app/heroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection: React.FC = () => {
   return (
     <div className="hero-section">
       <div className="content-container">
-      <h1 className="text-4xl lg:text-5xl font-semibold leading-tight text-center mx-auto px-4 lg:px-0 max-w-screen-lg">
+        <h1 className="text-4xl lg:text-5xl font-semibold leading-tight text-center mx-auto px-4 lg:px-0 max-w-screen-lg">
           <span>Unlock Your Financial Potential,</span>
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">
             Gamify Your Learning Journey
@@ -20,54 +20,36 @@ const HeroSection: React.FC = () => {
         </p>
         <SignUpButton />
       </div>
-      <motion.img
-                src="/heroIcons/Board.png"
-                alt="Descriptive alt text"
-                className="backdrop board"
-                initial={{ y: 0, opacity: 1 }}
-                whileHover={{ y: -10, opacity: 0.8 }}
-                transition={{ type: "spring", stiffness: 300 }}
-            />
-            <motion.img
-                src="/heroIcons/trophy.png"
-                alt="Descriptive alt text"
-                className="backdrop trophy"
-                initial={{ y: 0, opacity: 1 }}
-                whileHover={{ y: -10, opacity: 0.8 }}
-                transition={{ type: "spring", stiffness: 300 }}
-            />
-            <motion.img
-                src="/heroIcons/Online-Study.png"
-                alt="Descriptive alt text"
-                className="backdrop study"
-                initial={{ y: 0, opacity: 1 }}
-                whileHover={{ y: -10, opacity: 0.8 }}
-                transition={{ type: "spring", stiffness: 300 }}
-            />
-            <motion.img
-                src="/heroIcons/Star-1.png"
-                alt="Descriptive alt text"
-                className="backdrop star"
-                initial={{ y: 0, opacity: 1 }}
-                whileHover={{ y: -10, opacity: 0.8 }}
-                transition={{ type: "spring", stiffness: 300 }}
-            />
-            <motion.img
-                src="/heroIcons/Calculator-1.png"
-                alt="Descriptive alt text"
-                className="backdrop calculator"
-                initial={{ y: 0, opacity: 1 }}
-                whileHover={{ y: -10, opacity: 0.8 }}
-                transition={{ type: "spring", stiffness: 300 }}
-            />
-            <motion.img
-                src="/heroIcons/Card-1.png"
-                alt="Descriptive alt text"
-                className="backdrop card"
-                initial={{ y: 0, opacity: 1 }}
-                whileHover={{ y: -10, opacity: 0.8 }}
-                transition={{ type: "spring", stiffness: 300 }}
-            />
+      <img
+        src="/heroIcons/Board.png"
+        alt="Descriptive alt text"
+        className="backdrop board"
+      />
+      <img
+        src="/heroIcons/trophy.png"
+        alt="Descriptive alt text"
+        className="backdrop trophy"
+      />
+      <img
+        src="/heroIcons/Online-Study.png"
+        alt="Descriptive alt text"
+        className="backdrop study"
+      />
+      <img
+        src="/heroIcons/Star-1.png"
+        alt="Descriptive alt text"
+        className="backdrop star"
+      />
+      <img
+        src="/heroIcons/Calculator-1.png"
+        alt="Descriptive alt text"
+        className="backdrop calculator"
+      />
+      <img
+        src="/heroIcons/Card-1.png"
+        alt="Descriptive alt text"
+        className="backdrop card"
+      />
     </div>
   );
 };
