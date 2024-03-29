66ea3bad7643a006257086b08a4d0352412b67e7
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-03-28 22:51:39 -0700

    Navbar fix?

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,7 +13,9 @@ export default function Navbar() {
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
-      if (currentScrollY > lastScrollY) {
+      if (currentScrollY <= 0) {
+        setIsVisible(true);
+      } else if (currentScrollY > lastScrollY) {
         setIsVisible(false);
       } else {
         setIsVisible(true);
@@ -27,11 +29,11 @@ export default function Navbar() {
   return (
     <>
       <nav
-        className={`h-[120px] z-[9999] drop-shadow-lg w-full flex items-center justify-between bg-surface-primary px-24 fixed top-0 transition-all duration-300 ${
+        className={`fixed top-0 z-[9999] flex h-[80px] w-full items-center justify-between bg-surface-primary px-24 drop-shadow-[0_4px_40px_rgba(0,0,0,0.06)] transition-all duration-300 md:h-[120px] ${
           isVisible ? "translate-y-0" : "-translate-y-full"
         }`}
       >
-        <div className="flex items-center md:justify-start w-full justify-center">
+        <div className="flex w-full items-center justify-center md:justify-start">
           <Image
             src="/Effinity-Logo.png"
             width={161}
@@ -39,33 +41,33 @@ export default function Navbar() {
             alt="Effinity Logo"
             className=""
           />
-          <div className="w-[2px] h-7 bg-black mx-8 hidden md:block" />
-          <div className=" items-center flex-row gap-x-8 hidden md:flex">
+          <div className="mx-8 hidden h-7 w-[2px] bg-black md:block" />
+          <div className=" hidden flex-row items-center gap-x-8 md:flex">
             <Link
               target="_blank"
               href="https://www.linkedin.com/company/learneffinity/"
-              className="text-icon-primary hover:text-icon-primary/70 transition-all duration-300 ease-in-out"
+              className="text-icon-primary transition-all duration-300 ease-in-out hover:text-icon-primary/70"
             >
               <AiFillLinkedin size={24} />
             </Link>
             <Link
               target="_blank"
               href="https://discord.gg/mpBfqw5BG8"
-              className="text-icon-primary hover:text-icon-primary/70 transition-all duration-300 ease-in-out"
+              className="text-icon-primary transition-all duration-300 ease-in-out hover:text-icon-primary/70"
             >
               <FaDiscord size={24} />
             </Link>
             <Link
               target="_blank"
               href="https://www.instagram.com/learneffinity/"
-              className="text-icon-primary hover:text-icon-primary/70 transition-all duration-300 ease-in-out"
+              className="text-icon-primary transition-all duration-300 ease-in-out hover:text-icon-primary/70"
             >
               <FaInstagram size={24} />
             </Link>
             <Link
               target="_blank"
               href="https://twitter.com/learneffinity"
-              className="text-icon-primary hover:text-icon-primary/70 transition-all duration-300 ease-in-out"
+              className="text-icon-primary transition-all duration-300 ease-in-out hover:text-icon-primary/70"
             >
               <FaXTwitter size={24} />
             </Link>
@@ -73,7 +75,7 @@ export default function Navbar() {
         </div>
         <a
           href="#cta"
-          className="w-[158px] h-[54px] bg-brand-accent text-white rounded-xl justify-center items-center font-semibold hover:bg-brand-primary transition-all duration-300 ease-in-out hidden md:flex"
+          className="hidden h-[54px] w-[158px] items-center justify-center rounded-xl bg-brand-accent font-semibold text-white transition-all duration-300 ease-in-out hover:bg-brand-primary md:flex"
         >
           Join Beta!
         </a>
