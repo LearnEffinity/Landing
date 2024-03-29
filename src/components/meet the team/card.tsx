07bdacb60c095304b4ebaea93667a8c8a601dd2f
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-03-28 21:39:51 -0700

    Add LinkedIn icon to member card

--- src/components/meet the team/card.tsx
+++ src/components/meet the team/card.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { FaLinkedin } from "react-icons/fa";
 
 interface Member {
   id: number;
@@ -6,6 +8,7 @@ interface Member {
   role: string;
   description: string;
   image: string;
+  linkedin?: string;
   category: string;
 }
 
@@ -14,31 +17,41 @@ interface CardProps {
   isCurrentIndex: boolean;
 }
 
-const Card: React.FC<CardProps> = ({ member, isCurrentIndex }) => {
+const Card: React.FC<CardProps> = ({ member }) => {
   return (
     <div
-      className={`h-[624px] w-[392px] rounded-[26px] drop-shadow-lg ${
-        isCurrentIndex
-          ? "bg-gradient-to-r from-decorative-blue/50 to-decorative-red/50"
-          : ""
-      } p-[3px]`}
+      className={`group relative h-[624px] w-[392px] rounded-[26px] p-[3px] `}
     >
-      <div className="flex flex-col h-full px-6 pt-14 pb-20 w-full items-center rounded-3xl justify-center bg-surface-primary ">
-        <img
-          src={member.image}
-          key={member.id}
-          width={250}
-          height={250}
-          alt="Memoji"
-          className="mb-10"
-        />
-        <h3 className="font-semibold text-center text-2xl">{member.name}</h3>
-        <p className="font-medium text-lg text-text-tertiary mt-1">
-          {member.role}
-        </p>
-        <p className="text-center font-normal text-lg text-main-secondary max-w-[345px] max-h-[96px] mt-6 leading-9">
-          {member.description}
-        </p>
+      <div className="absolute inset-0 rounded-[26px] bg-gradient-to-r from-decorative-blue/50 to-decorative-red/50 opacity-0 transition-opacity duration-[300ms] ease-in-out group-hover:opacity-100"></div>
+      <div className="relative  z-10 flex h-full w-full flex-col items-center justify-center rounded-3xl bg-surface-primary px-6 pb-20 pt-14 hover:drop-shadow-[0_4px_30px_rgba(88,58,254,0.15)]">
+        <div className="flex h-[484px] flex-col items-center justify-center">
+          <img
+            src={member.image}
+            key={member.id}
+            width={250}
+            height={250}
+            alt="Memoji"
+            className="mb-10"
+          />
+          <div className="flex h-[192px] w-[344px] flex-col items-center justify-between">
+            <div>
+              <h3 className="text-center text-2xl font-semibold">
+                {member.name}
+              </h3>
+              <p className="mt-1 text-center text-lg font-medium text-text-tertiary">
+                {member.role}
+              </p>
+              <p className="text-main-secondary mt-6 w-full text-center text-lg font-normal leading-9">
+                {member.description}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-end justify-center pt-4">
+          <a href={member.linkedin} target="_blank">
+            <FaLinkedin className="h-7 w-7 cursor-pointer text-black transition-colors duration-300 hover:text-black/60" />
+          </a>
+        </div>
       </div>
     </div>
   );
